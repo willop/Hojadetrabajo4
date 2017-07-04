5e3bb4f960afd43c75b8a8f4bcf124405a6b722e
willop <wilfredp159@gmail.com>
2017-07-03 23:26:25 -0600

    s01.02
    Excercise-AddweatherList

---
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.support.annotation.StringDef;
+import android.support.v4.widget.TextViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO (1) Create a field to store the weather display TextView
+    TextView textvieee;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+
+        textvieee= (TextView) findViewById(R.id.tv_weather_data);
+
+
+
+        // TODO (3) Create an array of Strings that contain fake weather data
+        String[] palabras = {"Ahri - Mago","Oriana - Mago","Xayah - tirador","Aatox - top","Xin-Xao - jungla","Elise - Jungla","Rakan - Soporte","Twitch - tirador","Lucian - Tirador","Kled - top"};
+
+        // TODO (4) Append each String from the fake weather data array to the TextView
+       textvieee.append("CAMPEONES DE LOL \n");
+        for ( String ciclo : palabras) {
+            textvieee.append(ciclo +"\n\n\n");
+
+        }
+    }
+}
